699562a559a760e5530e8834bc333bfc2767bd74
Blasco <37955180+blascokoa@users.noreply.github.com>
2018-10-06 10:51:44 +0200

    Deleted Colorama

--- api_to_hash.py
+++ api_to_hash.py
@@ -1,13 +1,7 @@
 import hashlib
-from colorama import Fore
 
-RED = Fore.RED
-CYAN = Fore.LIGHTCYAN_EX
-RESET = Fore.RESET
-
-
-print(RED + "The system, is CASE SENSITIVE, please copy it as BitMax team sent to you" + RESET)
-key = input(CYAN + "Please copy and paste your PUBLIC API KEY: " + RESET)
+print("The system, is CASE SENSITIVE, please copy it as BitMax team sent to you")
+key = input("Please copy and paste your PUBLIC API KEY: ")
 
 hmac_key = hashlib.blake2b(key.encode("utf-8")).hexdigest()
 print(hmac_key)
